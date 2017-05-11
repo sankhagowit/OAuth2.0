0a9d92487b954088ad7851feef9f79982c8ede02
Bobby <bobby@sankhagowit.com>
2017-05-11 16:16:16 -0500

    feat: Add function to create anti forgery state tokens

--- database_setup.py
+++ database_setup.py
@@ -2,23 +2,23 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
- 
+
 Base = declarative_base()
 
 class Restaurant(Base):
     __tablename__ = 'restaurant'
-   
+
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'id'           : self.id,
-       }
- 
+        """Return object data in easily serializeable format"""
+        return {
+            'name'         : self.name,
+            'id'           : self.id,
+        }
+
 class MenuItem(Base):
     __tablename__ = 'menu_item'
 
@@ -34,18 +34,18 @@ class MenuItem(Base):
 
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'description'         : self.description,
-           'id'         : self.id,
-           'price'         : self.price,
-           'course'         : self.course,
-       }
+        """Return object data in easily serializeable format"""
+        return {
+            'name'         : self.name,
+            'description'         : self.description,
+            'id'         : self.id,
+            'price'         : self.price,
+            'course'         : self.course,
+        }
 
 
 
 engine = create_engine('sqlite:///restaurantmenu.db')
- 
+
 
 Base.metadata.create_all(engine)
